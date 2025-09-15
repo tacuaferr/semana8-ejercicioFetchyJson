09d05a3f7aec5c19c1a26b28c9c3b0e32e12e10f
Tacuabé Ferrari <inti.ferrari@fing.edu.uy>
2025-09-15 14:04:50 -0300

    implementado

--- js/script.js
+++ js/script.js
@@ -19,3 +19,11 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+async function getDatosJson(){
+  let promise = await fetch(DATA_URL),
+      res = await promise.json();
+
+  showData(res.students);
+}
+
+getDatosJson();
